19d4d978f8a4484dd6dd3daa71c163fe078544e2
usmt-cop <usmt.cop@gmail.com>
2022-04-09 19:00:27 +0530

    I have rnamed file to uiUx

---
+++ uiUx.js
@@ -0,0 +1,96 @@
+let amountVal,
+   statusVal,
+   EURval,
+   USDval,
+   INRval,
+   valNum = false;
+let currencySet = false;
+$(".currencySel").select(function () {
+   console.log("hi");
+});
+
+/*----------  It will fetch api on start of page,
+I can use static values late for concersions,
+  ----------*/
+
+$(document).ready(function () {
+   //$("button").click(function () {
+   $.get(
+      "http://api.exchangeratesapi.io/v1/latest?access_key=d78ccad0e0ba35c8175662df6c3d9930",
+      function (data, status, success) {
+         statusVal = status;
+         USDval = success["responseJSON"]["rates"]["USD"];
+         EURval = success["responseJSON"]["rates"]["EUR"] * 1.087583;
+         //INRval = success["responseJSON"]["rates"]["INR"]; // / (82.566554 * 1.087583);
+         console.log("usd", USDval, "inr", INRval, EURval);
+      }
+   );
+});
+
+/*----------  click button function  ----------*/
+
+$(".button-wala-2 button").click(function () {
+   let currencyTpye = $("#selectC").val();
+
+   /*----------  To check weather input is number or not  ----------*/
+   if ($.isNumeric(`${$("input").val()}`)) {
+      valNum = true;
+   } else {
+      valNum = false;
+   }
+
+   /*----------  to check weather Currency is selected or not  ----------*/
+
+   if (currencyTpye != "Select currency:") {
+      currencySet = true;
+   }
+
+   /**
+    *
+    * I have made a condition that input should be number and
+    *  currency must be choose to get go inside 'if'
+    *
+    */
+
+   if (currencySet && valNum) {
+      amountVal = $("input").val();
+      Ans = currencyAns(currencyTpye);
+      Ans *= $("input").val();
+      Ans = Math.floor(Ans * 100) / 100;
+      // console.log($("#test").text());
+      $("#test").text(`$${Ans}`);
+      $("#statusA")
+         .text(`${statusVal}`)
+         .css("color", "green")
+         .css("font-weight", "bold")
+         .css("text-transform", "capitalize")
+         .css("font-size", "1em");
+   } else {
+      $("#test").text("{{Value In USD}}");
+      $("#statusA")
+         .text(`FAILURE`)
+         .css("color", "red")
+         .css("font-weight", "bold")
+         .css("font-size", "1em");
+   }
+});
+/*----------  , cu  ----------*/
+
+/**
+ *
+ * It will return value of currency
+ * Currenty I have made static because of only 2 exchange on webpage
+ * you can make it dynamic easyly
+ *
+ */
+
+function currencyAns(currencyTpye) {
+   if (currencyTpye == "EUR") {
+      console.log(1);
+      return EURval;
+   } else {
+      console.log(2);
+      INRval = EURval / 75.92;
+      return INRval; // 82.566554; // 1.087583);
+   }
+}
